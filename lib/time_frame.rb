5e931ccd625223f201846d8151cf59eef30ef9eb
Danilo Sato <dtsato@gmail.com>
2011-06-30 11:30:22 -0300

    Splitting timeframe into classes

--- lib/time_frame.rb
+++ lib/time_frame.rb
@@ -1,23 +1,48 @@
-class TimeFrame < Struct.new(:start_date, :end_date)
+class TimeFrame
+  attr_reader :start_date, :end_date
+
+  class Today < TimeFrame
+    def initialize
+      date = DateTime.now
+      @start_date, @end_date = date.at_beginning_of_day, date.end_of_day
+    end
+  end
+
+  class Tomorrow < TimeFrame
+    def initialize
+      date = DateTime.now + 1
+      @start_date, @end_date = date.at_beginning_of_day, date.end_of_day
+    end
+  end
+
+  class ThisWeek < TimeFrame
+    def initialize
+      date = DateTime.now
+      @start_date, @end_date = date, (date + 6.days ).end_of_day
+    end
+  end
+
+  class Hour < TimeFrame
+    def initialize(hour)
+      hour = DateTime.strptime(hour, '%m/%d/%Y %H:%M')
+      @start_date, @end_date = hour, (hour + 1.hour - 1.second)
+    end
+  end
+
+  class Custom < TimeFrame
+    def initialize(start_date, end_date)
+      @start_date = start_date.blank? ? DateTime.now.beginning_of_week : DateTime.strptime(start_date, '%m/%d/%Y')
+      @end_date = end_date.blank? ? (DateTime.now.end_of_week - 2) : DateTime.strptime(end_date, '%m/%d/%Y').end_of_day
+    end
+  end
 
   def self.for(type, start_date, end_date, hour)
     case type
-    when 'tomorrow'
-      date = DateTime.now + 1
-      TimeFrame.new(date.at_beginning_of_day, date.end_of_day)
-    when 'this_week'
-      TimeFrame.new(DateTime.now, (DateTime.now + 6 ).end_of_day)
-    when 'custom'
-      start_date = start_date.blank? ? DateTime.now.beginning_of_week : DateTime.strptime(start_date, '%m/%d/%Y')
-      end_date = end_date.blank? ? (DateTime.now.end_of_week - 2) : DateTime.strptime(end_date, '%m/%d/%Y').end_of_day
-      TimeFrame.new(start_date, end_date)
-    when 'hour'
-      start_date = DateTime.strptime(hour, '%m/%d/%Y %H:%M')
-      end_date =  start_date + 1.hour-1.second
-      TimeFrame.new(start_date, end_date)
-    when 'today'
-      date = DateTime.now
-      TimeFrame.new(date.at_beginning_of_day, date.end_of_day)
+    when 'tomorrow' then Tomorrow.new
+    when 'this_week' then ThisWeek.new
+    when 'custom' then Custom.new(start_date, end_date)
+    when 'hour' then Hour.new(hour)
+    when 'today' then Today.new
     end
   end
 
--- spec/time_frame_spec.rb
+++ spec/time_frame_spec.rb
@@ -7,18 +7,18 @@ describe TimeFrame do
     DateTime.stub!(:now).and_return(@today)
   end
 
-  describe "today's event" do
+  describe TimeFrame::Today do
     it "should use beginning and end of day" do
-      timeframe = TimeFrame.for('today', nil, nil, nil)
+      timeframe = TimeFrame::Today.new
 
       timeframe.start_date.should == @today.at_beginning_of_day
       timeframe.end_date.should == @today.end_of_day
     end
   end
 
-  describe "tomorrow's event" do
+  describe TimeFrame::Tomorrow do
     it "should use beginning and end of tomorrow's day" do
-      timeframe = TimeFrame.for('tomorrow', nil, nil, nil)
+      timeframe = TimeFrame::Tomorrow.new
 
       tomorrow = @today + 1
       timeframe.start_date.should == tomorrow.at_beginning_of_day
@@ -26,38 +26,60 @@ describe TimeFrame do
     end
   end
 
-  describe "this week's events" do
+  describe TimeFrame::ThisWeek do
     it "should use 7 days period, starting today" do
-      timeframe = TimeFrame.for('this_week', nil, nil, nil)
+      timeframe = TimeFrame::ThisWeek.new
 
       timeframe.start_date.should == @today
       timeframe.end_date.should == (@today + 7.days) - 1.second
     end
   end
 
-  describe "custom date range" do
+  describe TimeFrame::Custom do
     it "should return events for specified date range, using end of day for end date" do
-      timeframe = TimeFrame.for('custom', '06/01/2010', '06/15/2010', nil)
+      timeframe = TimeFrame::Custom.new('06/01/2010', '06/15/2010')
 
       timeframe.start_date.should == DateTime.strptime('06/01/2010', '%m/%d/%Y')
       timeframe.end_date.should == DateTime.strptime('06/15/2010', '%m/%d/%Y').end_of_day
     end
 
     it "should default date range to this business week" do
-      timeframe = TimeFrame.for('custom', nil, nil, nil)
+      timeframe = TimeFrame::Custom.new(nil, nil)
 
       timeframe.start_date.should == @today.beginning_of_week
       timeframe.end_date.should == @today.end_of_week - 2.days
     end
   end
 
-  describe "events for a specific hour" do
+  describe TimeFrame::Hour do
     it "should use 1 hour interval starting from given hour" do
-      timeframe = TimeFrame.for('hour', nil, nil, "06/01/2010 15:00")
+      timeframe = TimeFrame::Hour.new("06/01/2010 15:00")
 
       hour = DateTime.strptime('06/01/2010 15:00', '%m/%d/%Y %H:%M')
       timeframe.start_date.should == hour
       timeframe.end_date.should == (hour + 1.hour) - 1.second
+    end
+  end
+
+  describe "parsing" do
+    it "should parse today's timeframe" do
+      TimeFrame.for('today', nil, nil, nil).should be_an_instance_of(TimeFrame::Today)
+    end
+
+    it "should parse tomorrow's timeframe" do
+      TimeFrame.for('tomorrow', nil, nil, nil).should be_an_instance_of(TimeFrame::Tomorrow)
+    end
+
+    it "should parse this week's timeframe" do
+      TimeFrame.for('this_week', nil, nil, nil).should be_an_instance_of(TimeFrame::ThisWeek)
+    end
+
+    it "should parse custom timeframe" do
+      TimeFrame.for('custom', nil, nil, nil).should be_an_instance_of(TimeFrame::Custom)
+    end
+
+    it "should parse hour's timeframe" do
+      TimeFrame.for('hour', nil, nil, '06/01/2010 15:00').should be_an_instance_of(TimeFrame::Hour)
     end
   end
 end
