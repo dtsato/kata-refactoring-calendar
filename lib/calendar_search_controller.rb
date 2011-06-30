fb93f0e6ec0aea9adc69dc7912e7d0d0590698e1
Danilo Sato <dtsato@gmail.com>
2011-06-30 09:47:17 -0300

    Using only one scope to search, move all logic to one place

--- lib/calendar_search_controller.rb
+++ lib/calendar_search_controller.rb
@@ -4,27 +4,26 @@ class CalendarSearchController
   attr_reader :events
   
   def index(params)
-    scope = Event
-    
     case params[:timeframe]
     when 'tomorrow'
-      scope = scope.between_day(DateTime.now + 1)
+      date = DateTime.now + 1
+      start_date = date.at_beginning_of_day
+      end_date =  start_date.end_of_day
     when 'this_week'
-      scope = scope.between_dates(DateTime.now, ( DateTime.now + 6 ).end_of_day)
+      start_date = DateTime.now
+      end_date = (DateTime.now + 6 ).end_of_day
     when 'custom'
-      if params[:start_date].blank?
-        params[:start_date] = DateTime.now.beginning_of_week.strftime('%m/%d/%Y')
-      end
-      if params[:end_date].blank?
-        params[:end_date] = (DateTime.now.end_of_week - 2).strftime('%m/%d/%Y')
-      end
-      scope = scope.between_dates(DateTime.strptime(params[:start_date], '%m/%d/%Y'), DateTime.strptime(params[:end_date], '%m/%d/%Y').end_of_day)
+      start_date = params[:start_date].blank? ? DateTime.now.beginning_of_week : DateTime.strptime(params[:start_date], '%m/%d/%Y')
+      end_date = params[:end_date].blank? ? (DateTime.now.end_of_week - 2) : DateTime.strptime(params[:end_date], '%m/%d/%Y').end_of_day
     when 'hour'
-      scope = scope.between_hour_on_day(DateTime.strptime(params[:hour], '%m/%d/%Y %H:%M'))
+      start_date = DateTime.strptime(params[:hour], '%m/%d/%Y %H:%M')
+      end_date =  start_date + 1.hour-1.second
     when 'today'
-      scope = scope.between_day(DateTime.now)
+      date = DateTime.now
+      start_date = date.at_beginning_of_day
+      end_date =  start_date.end_of_day
     end
     
-    @events = scope.all
+    @events = Event.between_dates(start_date, end_date)
   end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -2,43 +2,6 @@ require 'rspec'
 require 'event'
 
 describe Event do
-  describe "between_hour_on_day" do
-    before do
-      @event_beginning_of_hour = Event.create(:at => DateTime.strptime("02/16/2010 13:00:00", "%m/%d/%Y %H:%M:%S"))
-      @event_end_of_hour = Event.create(:at => DateTime.strptime("02/16/2010 13:59:59", "%m/%d/%Y %H:%M:%S"))
-      @after_event = Event.create(:at => DateTime.strptime("02/16/2010 14:00:00", "%m/%d/%Y %H:%M:%S"))
-    end
-
-    it "should return an event at the beginning of the hour" do
-      Event.between_hour_on_day(DateTime.strptime("02/16/2010 13:00", "%m/%d/%Y %H:%M")).should include(@event_beginning_of_hour)
-    end
-
-    it "should return an event at the end of the hour" do
-      Event.between_hour_on_day(DateTime.strptime("02/16/2010 13:00", "%m/%d/%Y %H:%M")).should include(@event_end_of_hour)
-    end
-
-    it "should not return an event after the hour window" do
-      Event.between_hour_on_day(DateTime.strptime("02/16/2010 13:00", "%m/%d/%Y %H:%M")).should_not include(@after_event)
-    end
-  end
-
-  describe "between_day" do
-    before do
-      @event_midnight = Event.create(:at => DateTime.strptime("02/16/2010", "%m/%d/%Y"))
-      @event_midday = Event.create(:at => DateTime.strptime("02/16/2010 13:00:00", "%m/%d/%Y %H:%M:%S"))
-      @event_midnight_tomorrow = Event.create(:at => DateTime.strptime("02/17/2010", "%m/%d/%Y"))
-    end
-
-    it "should return an event at the beginning of the day" do
-      Event.between_day(DateTime.strptime("02/16/2010", "%m/%d/%Y")).should include(@event_midday)
-    end
-    it "should return an event at midnight today" do
-      Event.between_day(DateTime.strptime("02/16/2010", "%m/%d/%Y")).should include(@event_midnight)
-    end
-    it "should not return an event at midnight tomorrow" do
-      Event.between_day(DateTime.strptime("02/16/2010", "%m/%d/%Y")).should_not include(@event_midnight_tomorrow)
-    end
-  end
 
   describe "between_dates" do
     it "should return events for a given date range" do
